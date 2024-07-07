38d3eb0c05a4a6c2dc83d716ddd032b7ef8e2f10
yashwantmadane14 <yashwantrajendra284@gmail.com>
2024-07-07 12:04:59 +0530

    routes Updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import StudentForm from './Pages/StudentForm'
 import StudentTable from './Pages/StudentTable'
 import StudentUdForm from './Pages/StudentUdForm'
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
@@ -10,11 +10,9 @@ function App() {
       <h1 className="text-3xl font-bold pt-5">
         School Management
       </h1 >
-    
-     
       <Routes>
-        <Route path='/' element={ <StudentTable />}></Route>
-        <Routes path='/addstudent' element={<StudentForm />}></Routes>
+        <Route path='/' element={<StudentTable />} />
+        <Route path='/addstudent' element={<StudentForm />}></Route>
         <Route path='/updatestudent' element={  <StudentUdForm />}></Route>
       </Routes>
     </>
