e9108ae2c6a3b9dcaebcccd8d282f0b5045eddf6
yashwantmadane14 <yashwantrajendra284@gmail.com>
2024-08-01 18:21:21 +0530

    Express Route Changed

--- server/Routes/studentRoute.js
+++ server/Routes/studentRoute.js
@@ -4,7 +4,7 @@ const studentCntrl = require("../Controllers/studentController")
 
 router.get("/getall", studentCntrl.getallstudents) //getting all the students
 router.get("/getbyid/:id",studentCntrl.getonestudentbyid) // getting student by id
-router.get("/get", studentCntrl.getonestudent) // getting student by name
+router.get("/get", studentCntrl.getallstudents) // getting student by name
 router.post("/create", studentCntrl.createstudent) // adding/creating student 
 router.delete("/delete/:id", studentCntrl.deletestudent) // delete student by id
 router.delete("/deleteall", studentCntrl.deleteallstudent) // delete all studnet
